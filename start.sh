d5d1fd5ac617ad1732c411914da29be7ccdddb3e
godric <godric.com@gmail.com>
2016-08-03 23:54:44 +0200

    Make start script handle multiple interfaces

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ if [ -z "$VPNADDR" -o -z "$VPNUSER" -o -z "$VPNPASS" ]; then
 fi
 
 # Setup masquerade, to allow using the container as a gateway
-eth0_net=$(ip a | grep eth0 | grep inet | awk '{print $2}')
-iptables -t nat -A POSTROUTING -s "$eth0_net" -j MASQUERADE
+for iface in $(ip a | grep eth | grep inet | awk '{print $2}'); do
+  iptables -t nat -A POSTROUTING -s "$iface" -j MASQUERADE
+done
 
 while [ true ]; do
   echo "------------ VPN Starts ------------"
